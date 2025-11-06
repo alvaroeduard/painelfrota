38c4fe4f03af561765be6f305a1aedb5ddc24a18
Alvaro Eduardo <alvaroeduardo297@gmail.com>
2025-11-06 20:49:23 -0300

    06/11/2025 20:49

--- pweb/settings.py
+++ pweb/settings.py
@@ -123,6 +123,7 @@ STATIC_URL = 'static/'
 # ADICIONE AS DUAS LINHAS ABAIXO
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [BASE_DIR / 'frota' / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
